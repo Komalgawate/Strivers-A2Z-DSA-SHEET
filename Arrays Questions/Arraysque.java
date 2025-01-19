daa0ce8263361930e3901ea54a9493206e0138eb
Komal Gawate <gawatekomal2@gmail.com>
2025-01-19 12:23:16 +0530

    Initial commit

---
+++ Arrays Questions/Arraysque.java
@@ -0,0 +1,105 @@
+
+//1. Largest Element in an Array
+// public class Arraysque {
+//     public static void main(String[] args) {
+//         int arr[]={2,5,1,8,9};
+//         int n=arr.length;
+//         System.out.println(findlargest(arr, n));
+//     }
+//     public static int findlargest(int arr[],int n){
+//         int largest=arr[0];
+//         for(int i=0;i<n;i++){
+//             if(arr[i]>largest)
+//             {
+//               largest=arr[i];
+//             }
+//         }
+//         return largest;
+//     }
+
+// }
+//2. Second Largest Element in an Array without sorting
+public class Arraysque {
+
+     public static void main(String[] args) {
+        
+     }
+     public static int secondlargest(){
+        
+     }
+}
+//3.Check if the array is sorted
+
+// 4.Remove duplicates from Sorted array
+
+// 5.Left Rotate an array by one place
+
+// 6.Left rotate an array by D places
+
+// 7.Move Zeros to end
+
+//8. Linear Search
+
+//9. Find the Union
+
+// 10.Find missing number in an array
+// 11.Maximum Consecutive One
+// 12.Find the number that appears once, and other numbers twice.
+
+// 13.Longest subarray with given sum K(positives)
+
+// 14.Longest subarray with sum K (Positives + Negatives)public class Arraysque {Lec 2: Medium
+
+//Medium
+// 15.2Sum Problem
+// 16.Sort an array of 0's 1's and 2's
+
+// 17.Majority Element (>n/2 times)
+
+// 18.Kadane's Algorithm, maximum subarray sum
+
+//19. Print subarray with maximum subarray sum (extended version of above problem)
+
+//20. Stock Buy and Sell
+
+// 21.Rearrange the array in alternating positive and negative items
+
+// 22.Next Permutation
+
+// 23.Leaders in an Array problem
+
+//24. Longest Consecutive Sequence in an Array
+
+//25. Set Matrix Zeros
+
+// 26.Rotate Matrix by 90 degrees
+
+// 27.Print the matrix in spiral manner
+
+// 28Count subarrays with given sum
+
+// Hard
+
+// Pascal's Triangle
+
+// Majority Element (n/3 times)
+
+// 3-Sum Problem
+
+// 4-Sum Problem
+
+// Largest Subarray with 0 Sum
+
+// Count number of subarrays with given xor K
+
+// Merge Overlapping Subintervals
+
+// Merge two sorted arrays without extra space
+
+// Find the repeating and missing number
+
+// Count Inversions
+
+// Reverse Pairs
+
+// Maximum Product Subarray
